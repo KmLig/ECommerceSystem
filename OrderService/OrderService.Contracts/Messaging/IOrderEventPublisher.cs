7ce7e4b8cf32efec4dd6c110e1f9256936590def
Khiem <phamxuankhiem1102@gmail.com>
2026-08-02 21:34:13 +0700

    Add IOrderEventPublisher interface to OrderService.Contracts for publishing order placed events, enhancing messaging capabilities.

---
+++ OrderService/OrderService.Contracts/Messaging/IOrderEventPublisher.cs
@@ -0,0 +1,8 @@
+using Messaging.Common.Events;
+namespace OrderService.Contracts.Messaging
+{
+    public interface IOrderEventPublisher
+    {
+        Task PublishOrderPlacedAsync(OrderPlacedEvent evt, string? correlationId = null);
+    }
+}
