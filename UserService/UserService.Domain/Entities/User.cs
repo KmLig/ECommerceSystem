de968ad35f0d28a3b53466eec7d7eb1980854e1f
Khiem <phamxuankhiem1102@gmail.com>
2026-08-02 17:00:30 +0700

    Refactor User entity in UserService.Domain to use Guid for Id, update property names for consistency, and add unique index on Email

--- UserService/UserService.Domain/Entities/User.cs
+++ UserService/UserService.Domain/Entities/User.cs
@@ -1,21 +1,19 @@
+using Microsoft.EntityFrameworkCore;
 namespace UserService.Domain.Entities
 {
+    [Index(nameof(Email), Name = "Index_Email_Unique", IsUnique = true)]
     public class User
     {
-        public int Id { get; set; }
-        public string Username { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
-        public string PasswordHash { get; set; } = string.Empty;
-        public string PhoneNumber { get; set; } = string.Empty;
-        public string FullName { get; set; } = string.Empty;
-        public string? ProfilePictureUrl { get; set; }
-        public bool IsEmailConfirmed { get; set; } = false;
-        public bool IsActive { get; set; } = true;
-        
-        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public Guid Id { get; set; }
+        public string? UserName { get; set; } = null!;
+        public string? Email { get; set; } = null!;
+        public bool IsEmailConfirmed { get; set; }
+        public bool IsActive { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? FullName { get; set; }
+        public string? ProfilePhotoUrl { get; set; }
+        public DateTime CreatedAt { get; set; }
         public DateTime? LastLoginAt { get; set; }
-
-        // Navigation Property (1 User có nhiều Address)
-        public ICollection<Address> Addresses { get; set; } = new List<Address>();
+        public List<Address> Addresses { get; set; } = new();
     }
 }
